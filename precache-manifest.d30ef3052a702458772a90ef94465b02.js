adaccff3bd72c6f76da7af8af659ec1a038e3e43
Balanagu Harsha Vardhan <t-bavard@microsoft.com>
2020-05-20 20:13:05 +0530

    Updates

---
+++ precache-manifest.d30ef3052a702458772a90ef94465b02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7328044fd66e73f1302b96923570bb",
+    "url": "/harsha130699/tobedeleted/index.html"
+  },
+  {
+    "revision": "9d263edac0133b1b93b6",
+    "url": "/harsha130699/tobedeleted/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "361b70a921524812fcbd",
+    "url": "/harsha130699/tobedeleted/static/js/2.ec84887b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/harsha130699/tobedeleted/static/js/2.ec84887b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d263edac0133b1b93b6",
+    "url": "/harsha130699/tobedeleted/static/js/main.940b3518.chunk.js"
+  },
+  {
+    "revision": "9dc3df8ff4a2b5e9062b",
+    "url": "/harsha130699/tobedeleted/static/js/runtime-main.09a41c66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/harsha130699/tobedeleted/static/media/logo.5d5d9eef.svg"
+  }
+]);
